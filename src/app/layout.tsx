05565198441cdaa8017a5250c9eba214d3ef84a7
Finn <85395498+fklk@users.noreply.github.com>
2024-02-08 14:03:15 +0100

    style(ui): remove unnecessary class

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
     return (
         <html lang="en">
             <body
-                className={`${inter.className} min-h-screen container flex justify-center items-center`}
+                className={`${inter.className} min-h-screen container flex justify-center`}
             >
                 {children}
             </body>
