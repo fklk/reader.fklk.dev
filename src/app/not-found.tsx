8a52912627d7ba7c3d7b3948b830715aa757c1a6
Finn <85395498+fklk@users.noreply.github.com>
2024-02-25 23:35:51 +0100

    feat(pages): add placeholder not-found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+export default function NotFound() {
+    return (
+        <div className="w-screen h-screen flex items-center justify-center">
+            <h1 className="text-4xl font-semibold">
+                The requested resource does not exist or you are unauthorized.
+            </h1>
+        </div>
+    );
+}
