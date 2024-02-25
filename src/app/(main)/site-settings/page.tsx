655cddaa3f1466d2a5b42bc82aabca7a70916303
Finn <85395498+fklk@users.noreply.github.com>
2024-02-25 23:29:14 +0100

    feat(pages): add placeholder SiteSettings page

---
+++ src/app/(main)/site-settings/page.tsx
@@ -0,0 +1,10 @@
+export default function SiteSettings() {
+    // TODO: Add delete comment functionality for admin users on hover
+    // TODO: List all genres and edit/add/delete functionality
+    // TODO: List all users with email and username, add/delete/Edit
+    // TODO: Configure which novels to showcase on /home
+    // TODO: List all novels (table) shadcn? And add/remove/edit/... functionality
+    // TODO: List all chapters (tabele) shadcn? Add/remove/edit... functionality
+    // TODO: List all comments (tabele) shadcn? Add/remove/edit... functionality
+    return <div>page</div>;
+}
