c53e05f79d38238ce151eb7867c2f978d8350d78
Finn <85395498+fklk@users.noreply.github.com>
2024-02-25 23:23:58 +0100

    feat(pages): add popular page

---
+++ src/app/(main)/popular/page.tsx
@@ -0,0 +1,24 @@
+import { api } from "@/trpc/server";
+import React from "react";
+import NovelCard from "../_components/novel-card";
+
+export default async function PopularPage() {
+    const novelIdsByPopularity = await api.novel.getAllByPopularity.query({
+        limit: 16,
+    });
+
+    return (
+        <div className="flex flex-col gap-6 mt-8">
+            <h1 className="text-3xl font-bold">Popular novels</h1>
+            <div className="flex gap-8 pt-2 pb-8 px-2 flex-wrap">
+                {novelIdsByPopularity?.map(novelId => (
+                    <NovelCard
+                        key={novelId}
+                        novelId={novelId}
+                        redirectTo="NOVEL_PAGE"
+                    />
+                ))}
+            </div>
+        </div>
+    );
+}
