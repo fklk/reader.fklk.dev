b946e30f9d72932835cfc100f44ca4ab59273332
Finn <85395498+fklk@users.noreply.github.com>
2024-02-25 23:23:18 +0100

    feat(pages): add browse page

---
+++ src/app/(main)/browse/_components/selector.tsx
@@ -0,0 +1,126 @@
+"use client";
+
+import { Badge } from "@/app/_components/ui/badge";
+import { Button } from "@/app/_components/ui/button";
+import {
+    Command,
+    CommandEmpty,
+    CommandGroup,
+    CommandInput,
+    CommandItem,
+} from "@/app/_components/ui/command";
+import {
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+} from "@/app/_components/ui/popover";
+import { capitalize, cn } from "@/lib/utils";
+import { Check, ChevronsUpDown } from "lucide-react";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useCallback, useState } from "react";
+
+type SelectorProps = {
+    items: any[];
+    name: string;
+    errorMessage: string;
+    defaultValue: string;
+    urlParam?: string;
+    onSelect?: (_: string) => void;
+};
+
+export default function Selector(props: SelectorProps) {
+    const [open, setOpen] = useState<boolean>(false);
+    const [value, setValue] = useState<string>(props.defaultValue);
+
+    const router = useRouter();
+    const pathname = usePathname();
+    const searchParams = useSearchParams();
+
+    const createQueryString = useCallback(
+        (name: string, value: string) => {
+            const params = new URLSearchParams(searchParams.toString());
+
+            if (params.has(name)) {
+                params.delete(name);
+            } else {
+                params.set(name, value);
+            }
+
+            return params.toString();
+        },
+        [searchParams]
+    );
+
+    const handleSelectChange = (currentValue: string) => {
+        setValue(currentValue === value ? "" : currentValue);
+        setOpen(false);
+
+        if (props.onSelect) {
+            props.onSelect(currentValue);
+        }
+
+        if (props.urlParam) {
+            router.push(
+                pathname + "?" + createQueryString(props.urlParam, currentValue)
+            );
+        }
+    };
+
+    return (
+        <Popover
+            open={open}
+            onOpenChange={setOpen}
+        >
+            <PopoverTrigger asChild>
+                <Button
+                    variant="outline"
+                    name={props.name}
+                    role="combobox"
+                    aria-expanded={open}
+                    className="w-[200px] justify-between bg-secondary focus-visible:bg-background"
+                >
+                    {value ? (
+                        <div className="flex items-center gap-2">
+                            {capitalize(props.name)}:{" "}
+                            <Badge className="font-semibold">
+                                {props.items.find(
+                                    item => item.toLowerCase() === value
+                                )}
+                            </Badge>
+                        </div>
+                    ) : (
+                        `Select ${props.name} ...`
+                    )}
+                    <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+                </Button>
+            </PopoverTrigger>
+            <PopoverContent className="w-[200px] p-0">
+                <Command>
+                    <CommandInput placeholder={`Search ${props.name}...`} />
+                    <CommandEmpty>{props.errorMessage}</CommandEmpty>
+                    <CommandGroup>
+                        {props.items?.map(item => (
+                            <CommandItem
+                                key={item}
+                                value={item}
+                                onSelect={currentValue => {
+                                    handleSelectChange(currentValue);
+                                }}
+                            >
+                                <Check
+                                    className={cn(
+                                        "mr-2 h-4 w-4",
+                                        value === item.toLowerCase()
+                                            ? "opacity-100"
+                                            : "opacity-0"
+                                    )}
+                                />
+                                {item}
+                            </CommandItem>
+                        ))}
+                    </CommandGroup>
+                </Command>
+            </PopoverContent>
+        </Popover>
+    );
+}
---
+++ src/app/(main)/browse/page.tsx
@@ -0,0 +1,74 @@
+import { api } from "@/trpc/server";
+import NovelCard from "../_components/novel-card";
+import Selector from "./_components/selector";
+
+type BrowsePageProps = {
+    searchParams: {
+        genre: string | undefined;
+        author: string | undefined;
+        minChapter: string | undefined;
+    };
+};
+
+export default async function BrowsePage(props: BrowsePageProps) {
+    const genres = await api.genre.getAll.query();
+    const authors = await api.user.getAllAuthors.query();
+    const minMax = await api.chapter.getMinMax.query();
+
+    const novelIdsByPopularity = await api.novel.getAllByPopularity.query({
+        genre: props.searchParams.genre,
+        author: props.searchParams.author,
+        minChapter: props.searchParams.minChapter
+            ? parseInt(props.searchParams.minChapter)
+            : undefined,
+    });
+
+    return (
+        <div className="flex flex-col flex-grow gap-8 pb-8">
+            <div className="flex flex-col gap-4">
+                <h1 className="text-3xl font-bold">Filter</h1>
+                <div className="flex gap-8">
+                    <Selector
+                        name="genre"
+                        urlParam="genre"
+                        items={genres.map(genre => genre.name)}
+                        errorMessage="No genres found"
+                        defaultValue={props.searchParams.genre ?? ""}
+                    />
+                    <Selector
+                        name="author"
+                        urlParam="author"
+                        items={authors.map(author => author.handle)}
+                        errorMessage="No authors found"
+                        defaultValue={props.searchParams.author ?? ""}
+                    />
+                    <Selector
+                        name="min chapters"
+                        urlParam="minChapter"
+                        items={Array.from({
+                            length: minMax.maxChapters - minMax.minChapters + 1,
+                        }).map((_, i) => i.toString())}
+                        errorMessage="Invalid number"
+                        defaultValue={props.searchParams.minChapter ?? ""}
+                    />
+                </div>
+            </div>
+            <div className="flex flex-grow gap-8 flex-wrap">
+                {novelIdsByPopularity.map(novelId => (
+                    <NovelCard
+                        key={novelId}
+                        novelId={novelId}
+                        redirectTo="NOVEL_PAGE"
+                    />
+                ))}
+                {novelIdsByPopularity.length === 0 ? (
+                    <div className="w-full h-full flex items-center justify-center">
+                        <h3 className="text-xl text-muted-foreground">
+                            No novels meet the given criteria.
+                        </h3>
+                    </div>
+                ) : null}
+            </div>
+        </div>
+    );
+}
