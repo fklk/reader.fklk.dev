894be4d01f957984109ede35e9653c84fcd088b3
fklk <finn.koelker@outlookde.>
2024-03-13 20:49:24 +0100

    chore(api): add missing attribute in create call

--- src/server/api/routers/novel.ts
+++ src/server/api/routers/novel.ts
@@ -48,6 +48,7 @@ export const novelRouter = createTRPCRouter({
                 data: {
                     name: input.name,
                     description: input.description,
+                    imgPath: "",
                     genre: {
                         connect: {
                             id: genre!.id,
